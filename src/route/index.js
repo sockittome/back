66deed704a9b03047bb2aae93691d3e527002041
strawbee <yuejiao.hou@gmail.com>
2018-03-19 09:13:40 -0700

    more starter code

---
+++ src/route/index.js
@@ -0,0 +1,20 @@
+import { Router } from 'express';
+import cors from 'cors';
+import bindResponse from '../middleware/bind-response';
+// import routerAuth from './route-auth';
+// import routerProfile from './route-profile';
+// import routerGames from './route-games';
+// import errorHandler from '../middleware/error-handler';
+
+export default new Router()
+  .use([
+    cors({
+      origin: process.env.CORS_ORIGIN,
+      credentials: true,
+    }),
+    bindResponse,
+    // routerAuth,
+    // routerGames,
+    // routerProfile,
+    // errorHandler,
+  ]);
