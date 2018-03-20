cbaf48e6e49e82c8712fc4d1fbcf786ba371b8db
Dean <dean.murphy90@gmail.com>
2018-03-20 14:09:03 -0700

    auth post and get route testing working and coverage looks dope

--- src/middleware/error-handler.js
+++ src/middleware/error-handler.js
@@ -7,8 +7,6 @@ module.exports = function (err, res) {
     case msg.includes('path error'): return res.status(404).send(`${err.name}: ${err.message}`);
     case msg.includes('objectid failed'): return res.status(404).send(`${err.name}: ${err.message}`);
     case msg.includes('duplicate key'): return res.status(409).send(`${err.name}: ${err.message}`);
-      // case msg.includes('no lyrics found'): return res.status(404).send(`${err.name}: ${err.message}`);
-      // case msg.includes('lyrics sample'): return res.status(400).send(`${err.name}: ${err.message}`);
     default: return res.status(500).send(`${err.name}: ${err.message}`);
   }
 };
