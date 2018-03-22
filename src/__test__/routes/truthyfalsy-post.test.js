9b711f1c4e7c9e5802ea03063f1f1b6d6d7b6e75
Melanie Downing <sayanything830@gmail.com>
2018-03-22 10:06:47 -0700

    working on tests

--- src/__test__/routes/truthyfalsy-post.test.js
+++ src/__test__/routes/truthyfalsy-post.test.js
@@ -7,7 +7,7 @@ const mocks = require('../lib/mocks');
 require('jest');
 
 const PORT = process.env.PORT;
-const ENDPOINT_PROFILE = `:${PORT}/api/v1/truthyfalsy`;
+// const ENDPOINT_PROFILE = `${PORT}/api/v1/truthyfalsy`;
 
 describe('POST /api/v1/truthyfalsy', function() {
   beforeAll(server.start);
@@ -17,14 +17,22 @@ describe('POST /api/v1/truthyfalsy', function() {
     it('Should return a status code of 201', () => {
       mocks.truthyfalsy.createOne().then(mock => this.mockGame = mock)
         .then(createdGame => {
-          return superagent.post(ENDPOINT_PROFILE)
+          return superagent.post(`:${PORT}/api/v1/truthyfalsy`)
             .set('Authorization', `Bearer ${this.mockGame.user.token}`)
             .send(this.mockGame)
             .then(res => {
               this.response = res;
             });
-          expect(this.response.status).toBe(201);
+          expect(this.response.status).toBe(20);
         });
+    });
+  });
+  describe('Invalid route', () => {
+    it('should respond with a 404 not found', () => {
+      return superagent.post(`${PORT}/api/v1/falsytruthy`)
+        .catch(err => this.error = err);
+      console.log(this.error);
+      expect(this.error.status).toEqual(404);
     });
   });
 });
