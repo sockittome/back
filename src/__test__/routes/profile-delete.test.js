46162f108eb2f373c82297aa8108501c4122f783
Melanie Downing <sayanything830@gmail.com>
2018-03-22 13:39:16 -0700

    working tests

--- src/__test__/routes/profile-delete.test.js
+++ src/__test__/routes/profile-delete.test.js
@@ -1,51 +1,44 @@
 'use strict';
 
 const superagent = require('superagent');
-const faker = require('faker');
-const server = require('../../lib/http-server');
+
 const mocks = require('../lib/mocks');
+const server = require('../../lib/http-server');
+require('jest');
 
 const PORT = process.env.PORT;
-const ENDPOINT_PROFILE = `:${PORT}/api/v1/profile`;
+const PROFILE_ENDPOINT = `:${PORT}/api/v1/profile`;
 
-describe('DELETE /api/v1/profile/:id', function() {
+describe('DELETE /api/v1/profile', function() {
   beforeAll(server.start);
   afterAll(server.stop);
+  afterAll(mocks.auth.removeAll);
+  afterAll(mocks.profile.removeAll);
+  afterAll(mocks.truthyfalsy.removeAll);
 
-  describe('Valid Routes', () => {
-    it('Should respond with a status code of 204', () => {
-      mocks.profile.createOne().then(data => this.mockProfile = data)
-        .then(newProfile => {
-          return superagent.delete(`${ENDPOINT_PROFILE}/${newProfile._id}`)
-            .set('Authorization', `Bearer ${newProfile.user.token}`)
-            .then(res => {
-              expect(res.status).toEqual(204);
-            });
+  describe('Valid route', () => {
+    beforeAll(() => {
+      return mocks.profile.createOne()
+        .then(profile => this.mockProfile = profile);
+    });
+    it('should delete a profile', () => {
+      return superagent.delete(`${PROFILE_ENDPOINT}/${this.mockProfile.profile._id}`)
+        .set('Authorization', `Bearer ${this.mockProfile.user.token}`)
+        .then(res => expect(res.status).toEqual(204));
+    });
+  });
+  describe('Invalid route', () => {
+    it('should return 404 not found', () => {
+      return superagent.delete(`:${PORT}/api/v1/not my profile`)
+        .catch(err => expect(err.status).toEqual(404));
+    });
+    it('should return 401 not authorized', () => {
+      return mocks.profile.createOne()
+        .then(mockProfile => {
+          return superagent.delete(`${PROFILE_ENDPOINT}/${mockProfile._id}`)
+            .set('Authorization', `Bearer BAD_TOKEN`)
+            .catch(err => expect(err.status).toEqual(401));
         });
     });
   });
-
-  describe('Invalid Routes', () => {
-    it('Should respond with an authorization error', () => {
-      mocks.profile.createOne().then(data => this.mockProfile = data)
-        .then(newProfile => {
-          return superagent.delete(`${ENDPOINT_PROFILE}/${newProfile._id}`)
-            .catch(err => {
-              this.error = err;
-              expect(err.response.text).toMatch(/Auth/);
-            });
-        });
-    });
-    it('Should respond with a status code of 404 when given a bad path', () => {
-      mocks.profile.createOne().then(data => this.mockProfile = data)
-        .then(newProfile => {
-          return superagent.delete(`${ENDPOINT_PROFILE}/tim`)
-            .catch(err => {
-              this.error = err;
-              expect(err.status).toBe(404);
-            });
-        });
-    });
-  });
-
 });
--- src/__test__/routes/profile-get.test.js
+++ src/__test__/routes/profile-get.test.js
@@ -1,53 +1,44 @@
 'use strict';
 
 const superagent = require('superagent');
-const faker = require('faker');
-const server = require('../../lib/http-server');
+
 const mocks = require('../lib/mocks');
+const server = require('../../lib/http-server');
+require('jest');
 
 const PORT = process.env.PORT;
-const ENDPOINT_PROFILE = `:${PORT}/api/v1/profile`;
+const PROFILE_ENDPOINT = `:${PORT}/api/v1/profile`;
 
-describe('GET /api/v1/profile/:_id', function() {
+describe('GET /api/v1/profile', function() {
   beforeAll(server.start);
   afterAll(server.stop);
+  afterAll(mocks.auth.removeAll);
+  afterAll(mocks.profile.removeAll);
+  afterAll(mocks.truthyfalsy.removeAll);
 
-  describe('Valid Routes', () => {
-    it('Should respond with a status code of 200', () => {
-      mocks.profile.createOne().then(data => this.mockProfile = data)
-        .then(newProfile => {
-          return superagent.get(`${ENDPOINT_PROFILE}/${newProfile._id}`)
-            .set('Authorization', `Bearer ${newProfile.user.token}`)
-            .then(res => {
-              expect(res.status).toEqual(200);
-              expect(Array.isArray(res.body)).toBeTruthy();
-            });
+  describe('Valid route', () => {
+    beforeAll(() => {
+      return mocks.profile.createOne()
+        .then(profile => this.mockProfile = profile);
+    });
+    it('should get the profile of the authorized user', () => {
+      return superagent.get(`${PROFILE_ENDPOINT}/${this.mockProfile.user.user._id}`)
+        .set('Authorization', `Bearer ${this.mockProfile.user.token}`)
+        .then(res => expect(res.status).toEqual(200));
+    });
+  });
+  describe('Invalid route', () => {
+    it('should send a 404 not found', () => {
+      return superagent.get(`:${PORT}/api/v1/not my profile`)
+        .catch(err => expect(err.status).toEqual(404));
+    });
+    it('should return 401 unauthorized', () => {
+      return mocks.profile.createOne()
+        .then(mockProfile => {
+          return superagent.get(`${PROFILE_ENDPOINT}/${mockProfile._id}`)
+            .set('Authorization', `Bearer BAD_TOKEN`)
+            .catch(err => expect(err.status).toEqual(401));
         });
     });
   });
-
-  describe('Invalid Routes', () => {
-    it('Should respond with an authorization error', () => {
-      mocks.profile.createOne().then(data => this.mockProfile = data)
-        .then(newProfile => {
-          return superagent.get(`${ENDPOINT_PROFILE}/${newProfile._id}`)
-            .catch(err => {
-              this.error = err;
-              expect(err.response.text).toMatch(/Auth/);
-            });
-        });
-    });
-    it('Should respond with a status code of 404 when given a bad path', () => {
-      mocks.profile.createOne().then(data => this.mockProfile = data)
-        .then(newProfile => {
-          return superagent.get(`${ENDPOINT_PROFILE}/tim`)
-            .set('Authorization', `Bearer ${newProfile.user.token}`)
-            .catch(err => {
-              this.error = err;
-              expect(err.status).toBe(404);
-            });
-        });
-    });
-  });
-
 });
